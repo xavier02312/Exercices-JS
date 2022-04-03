0d3d7c028c03ca744bd349f283b49fd2c4366c71
WriteCode <56721751+writecode-app@users.noreply.github.com>
2022-04-03 18:39:25 +0200

    Create exercice_1.js

---
+++ exercice_1.js
@@ -0,0 +1,24 @@
+/**
+ * Faites une copie de ce dépôt sur votre compte GitHub avant de commencer.
+ */
+
+/**
+ * Utiliser le tableau suivant
+ */
+let numbers = [12, 56, 83, 45, 7, 56, 4, 14, 29];
+
+/**
+ * 1. Multiplier par 2 chaque valeur de ce tableau et afficher les résultats dans la console.
+ */
+
+
+/**
+ * 2. Afficher la moyenne du tableau en utilisant la fonction reduce().
+ *    Si la moyenne est une décimale, limité le nombre après la virgule à 2.
+ */
+
+
+/**
+ * 3. Extraire tous les nombres impairs de ce tableau. Utiliser l'instruction filter()
+ */
+
