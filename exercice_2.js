313303dbaff953654c5ad11edb1bac3eafe84343
WriteCode <56721751+writecode-app@users.noreply.github.com>
2022-04-03 18:49:26 +0200

    Create exercice_2.js

---
+++ exercice_2.js
@@ -0,0 +1,28 @@
+/**
+ * Faites une copie de ce dépôt sur votre compte GitHub avant de commencer.
+ */
+
+/**
+ * Utiliser le tableau suivant
+ */
+const visiteurs = [
+  ['Gérard', 'Freschi', 'gfreschi0@discuz.net'],
+  ['Bendicty', 'Foster-Smith', 'bfostersmith1@mysql.com'],
+  ['Michelle', 'Draude', 'mdraude2@csmonitor.com'],
+  ['Cathee', 'Meineck', 'cmeineck3@t-online.de'],
+  ['Marianna', 'Coal', 'mcoal4@creativecommons.org'],
+];
+
+/**
+ * 1. Vérifier si le nom de famille "Meineck" existe dans le tableau. Afficher un message en conséquence.
+ */
+
+
+/**
+ * 2. Trier le tableau par ordre alphabétique selon les prénoms.
+ */
+
+
+/**
+ * 3. Créer un tableau contenant que les adresses e-mails en utilisant l'instruction map().
+ */
